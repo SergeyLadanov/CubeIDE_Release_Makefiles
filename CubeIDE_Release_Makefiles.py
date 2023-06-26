d0cb539f3aca5a04556a9bba790ef6181e69d956
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2023-06-26 22:53:21 +0300

    List of allowing files

--- CubeIDE_Release_Makefiles.py
+++ CubeIDE_Release_Makefiles.py
@@ -3,21 +3,35 @@ import shutil
 import sys
 
 
-SRC_DIR = "/Release"
+SRC_DIR = "/Debug"
 DST_DIR = "/Makefiles"
 
+# List of allowing endings of files
+AllowEndOfFilesList = [
+    ".mk",
+    "objects.list",
+    "makefile"
+]
 
 
+# Check end of file
+def CheckFile(f):
+    Status = True
+    for item in AllowEndOfFilesList:
+        if f[-len(item):] == str(item):
+            Status = False
+    return Status
+
+
+# Get current path
 path = os.path.realpath(os.path.dirname(sys.argv[0]))
 
 
-# ignore any files but files with '.mk, .list' extension and makefiles
+# Ignore any files but files with extension from list must be pass  
 ignore_func = lambda d, files: [
     f for f in files 
         if os.path.isfile(os.path.join(d, f)) 
-        and f[-3:] != '.mk' 
-        and f[-12:] != 'objects.list' 
-        and f[-8:] != 'makefile'
+        and CheckFile(f)
     ]
 
 if (os.path.isdir(path + DST_DIR)):
